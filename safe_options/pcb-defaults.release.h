b6b265a0da79c7432c364e09108acf81bf89b509
John Johnson <pcbgcode@pcbgcode.org>
2013-11-29 01:34:02 -0500

    Sensible default options.

--- safe_options/pcb-defaults.release.h
+++ safe_options/pcb-defaults.release.h
@@ -1,19 +1,19 @@
 //
 // Default values for generating gcode from a PCB.
 //
-// These settings were last changed with pcb-gcode-setup: 12/28/12 12:29 PM
+// These settings were last changed with pcb-gcode-setup: 11/29/13 1:32 AM
 //
 //
 // Changes you make in this file will be overwritten if you use pcb-gcode-setup.
 //
 
-int SINGLE_PASS = YES;
+int SINGLE_PASS = NO;
 real ISO_MIN = 0.001000;
 real ISO_MAX = 0.020000;
-real ISO_STEP = 0.003000;
+real ISO_STEP = 0.005000;
 
-int GENERATE_TOP_OUTLINES = NO;
-int GENERATE_TOP_DRILL = NO;
+int GENERATE_TOP_OUTLINES = YES;
+int GENERATE_TOP_DRILL = YES;
 int GENERATE_TOP_FILL = NO;
 
 int GENERATE_BOTTOM_OUTLINES = YES;
