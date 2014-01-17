e87527ac40deef8071337e0fd5ad3084bdeb75b4
John Johnson <pcbgcode@pcbgcode.org>
2014-01-17 13:09:10 -0500

    Bumped version.

--- rakefile.rb
+++ rakefile.rb
@@ -6,7 +6,7 @@ require 'pp'
 require 'rake/clean'
 
 # this will be improved later
-PCB_GCODE_VERSION = "3.6.1.3"
+PCB_GCODE_VERSION = "3.6.1.4"
 
 RELEASE_FILE = "~/Documents/pcb-gcode-#{PCB_GCODE_VERSION}.zip"
 
