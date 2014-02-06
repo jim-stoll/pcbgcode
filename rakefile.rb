ea9e3e791b6d987abd496e80834db4273c981fe4
Martin L. Marriott <martinlmarriott@hotmail.com>
2014-02-06 11:15:32 -0500

    Update docs and version number.

--- rakefile.rb
+++ rakefile.rb
@@ -6,7 +6,7 @@ require 'pp'
 require 'rake/clean'
 
 # this will be improved later
-PCB_GCODE_VERSION = "3.6.1.4"
+PCB_GCODE_VERSION = "3.6.1.5"
 
 RELEASE_FILE = "~/Documents/pcb-gcode-#{PCB_GCODE_VERSION}.zip"
 
